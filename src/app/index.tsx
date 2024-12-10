0945aa7e351d111c0b989bee41d565513b69caa9
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-12-10 08:36:37 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 24 }}>Hello React Native 🚀</Text>
+    </View>
+  );
+}
